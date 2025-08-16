f806b70afa2eb549ca078c5296ca8053ba5d3380
Batesoooooo <52887557+Batesoooooo@users.noreply.github.com>
2025-08-16 02:02:06 +0100

    Add cell customization feature (row/col spans, aspect ratio, cell menu)

---
+++ params/actions.py
@@ -0,0 +1,7 @@
+ACTIONS = {
+    # ... other actions ...
+    "customize_cell": {
+        "label": "Customize Cell",
+        "description": "Configure cell row/col spans and aspect ratio",
+    }
+}
